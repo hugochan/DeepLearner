9d7fc584b767cb29950bfa95951d22c450153868
hugochan <hugochan2013@gmail.com>
2017-03-20 15:11:08 -0400

    update

--- intro_to_dl_assignments/assign3/assign3.py
+++ intro_to_dl_assignments/assign3/assign3.py
@@ -9,8 +9,16 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
-label_lookup = dict(zip(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], range(10)))
+n_class = 10
 
+def dense_to_one_hot(labels_dense, num_classes):
+  """Convert class labels from scalars to one-hot vectors."""
+  num_labels = labels_dense.shape[0]
+  index_offset = np.arange(num_labels) * num_classes
+  labels_one_hot = np.zeros((num_labels, num_classes))
+  labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+
+  return labels_one_hot
 
 def revdict(d):
     """
@@ -37,7 +45,7 @@ def load_image(in_dir):
 
 def load_label(in_file):
     with open(in_file, 'r') as f:
-        labels = f.read().strip().split('\n')
+        labels = np.array([int(x) for x in f.read().strip().split('\n')])
 
     return labels
 
@@ -53,20 +61,20 @@ def load_weights(file):
 def save_weights(weights, file):
     try:
         with open(file, 'w') as f:
-            pickle.dump(weights, f)
+            pickle.dump(weights, f, protocol=2)
     except Exception as e:
         raise e
 
-def one_hot_encode(labels, label_lookup):
-    label_set = set(labels)
-    n_labels = len(label_set)
-    # label_lookup = dict(list(label_set), range(n_labels))
-    label_codes = np.eye(n_labels)
-    encoded_labels = []
-    for each in labels:
-        encoded_labels.append(label_codes[label_lookup[each]])
+# def one_hot_encode(labels, label_lookup):
+#     label_set = set(labels)
+#     n_labels = len(label_set)
+#     # label_lookup = dict(list(label_set), range(n_labels))
+#     label_codes = np.eye(n_labels)
+#     encoded_labels = []
+#     for each in labels:
+#         encoded_labels.append(label_codes[label_lookup[each]])
 
-    return np.r_[encoded_labels]
+#     return np.r_[encoded_labels]
 
 def shuffle_data(X_data, Y_data):
     idx = np.arange(X_data.shape[0])
@@ -76,23 +84,18 @@ def shuffle_data(X_data, Y_data):
 
     return X_data, Y_data
 
-def relu(X):
-    return tf.nn.relu(X)
-
-def softmax(X, axis):
-    f = tf.exp(X)
-
-    return tf.div(f, tf.reduce_sum(f, axis, keep_dims=True))
-
 def calc_loss(y, y_hat):
     return .5 * tf.reduce_sum(tf.squared_difference(y_hat, y)) / tf.cast(tf.shape(y)[0], tf.float32)
 
 def feedforward(X, W1, W10, W2, W20, W3, W30):
-    h1 = relu(tf.matmul(W1, X, transpose_a=True, transpose_b=True) + W10)
-    h2 = relu(tf.matmul(W2, h1, transpose_a=True) + W20)
-    y_hat = softmax(tf.matmul(W3, h2, transpose_a=True) + W30, 0)
+    # Hidden layer with RELU activation
+    h1 = tf.nn.relu(tf.add(tf.matmul(X, W1), tf.transpose(W10)))
+    # Hidden layer with RELU activation
+    h2 = tf.nn.relu(tf.add(tf.matmul(h1, W2), tf.transpose(W20)))
+    # Output layer with soft activation
+    y_hat = tf.nn.softmax(tf.add(tf.matmul(h2, W3), tf.transpose(W30)))
 
-    return tf.transpose(h1), tf.transpose(h2), tf.transpose(y_hat)
+    return h1, h2, y_hat
 
 def backprop(X, y, h1, h2, y_hat, W1, W10, W2, W20, W3, W30, lr):
     delta_y_hat = y_hat - y # bs * n_output
@@ -135,19 +138,12 @@ def backprop(X, y, h1, h2, y_hat, W1, W10, W2, W20, W3, W30, lr):
         delta_W1 += tf.multiply(tf.multiply(tf.reshape(X, (-1, n_input, 1)), mask), delta_h1_ext[:, i])
         delta_W10 += tf.multiply(tf.reshape(mask, (-1, n_h1, 1)), delta_h1_ext[:, i])
 
-    delta_W1 = tf.reduce_mean(delta_W1, 0)
-    delta_W10 = tf.reduce_mean(delta_W10, 0)
-    delta_W2 = tf.reduce_mean(delta_W2, 0)
-    delta_W20 = tf.reduce_mean(delta_W20, 0)
-    delta_W3 = tf.reduce_mean(delta_W3, 0)
-    delta_W30 = tf.reduce_mean(delta_W30, 0)
-
-    W1_update = W1.assign_sub(lr * delta_W1)
-    W10_update = W10.assign_sub(lr * delta_W10)
-    W2_update = W2.assign_sub(lr * delta_W2)
-    W20_update = W20.assign_sub(lr * delta_W20)
-    W3_update = W3.assign_sub(lr * delta_W3)
-    W30_update = W30.assign_sub(lr * delta_W30)
+    W1_update = W1.assign_sub(lr * tf.reduce_mean(delta_W1, 0))
+    W10_update = W10.assign_sub(lr * tf.reduce_mean(delta_W10, 0))
+    W2_update = W2.assign_sub(lr * tf.reduce_mean(delta_W2, 0))
+    W20_update = W20.assign_sub(lr * tf.reduce_mean(delta_W20, 0))
+    W3_update = W3.assign_sub(lr * tf.reduce_mean(delta_W3, 0))
+    W30_update = W30.assign_sub(lr * tf.reduce_mean(delta_W30, 0))
 
     return W1_update, W10_update, W2_update, W20_update, W3_update, W30_update
 
@@ -157,7 +153,7 @@ def next_batch(X, Y, batch_size):
         yield (X[i: i + batch_size], Y[i: i + batch_size])
 
 
-def run_dnn(train_X, train_Y, val_X, val_Y, h_dims, lr=1e-3, batch_size=50, max_epoch=1000, patience=10, shuffle=True, print_per_epoch=10):
+def run_dnn(train_X, train_Y, val_X, val_Y, h_dims, lr=1e-3, batch_size=50, max_epoch=1000, min_delta=1e-4, patience=10, shuffle=True, print_per_epoch=10):
     """Stochastic Gradient descent optimizer.
     """
     if len(h_dims) != 2:
@@ -166,74 +162,62 @@ def run_dnn(train_X, train_Y, val_X, val_Y, h_dims, lr=1e-3, batch_size=50, max_
 
     X = tf.placeholder(tf.float32, shape=[None, train_X.shape[1]], name='X')
     Y = tf.placeholder(tf.float32, shape=[None, train_Y.shape[1]], name='Y')
-    np.random.seed(0)
-    W1_val = 0.01 * np.random.randn(train_X.shape[1], h_dims[0]).astype('float32')
-    W10_val = np.zeros((h_dims[0], 1)).astype('float32')
-    W2_val = 0.01 * np.random.randn(h_dims[0], h_dims[1]).astype('float32')
-    W20_val = np.zeros((h_dims[1], 1)).astype('float32')
-    W3_val = 0.01 * np.random.randn(h_dims[1], train_Y.shape[1]).astype('float32')
-    W30_val = np.zeros((train_Y.shape[1], 1)).astype('float32')
-    W1, W10, W2, W20, W3, W30 = [tf.Variable(w) for w in [W1_val, W10_val, W2_val, W20_val, W3_val, W30_val]]
+    std = 0.1
+    seed = 1234
+    W1 = tf.Variable(tf.random_normal([train_X.shape[1], h_dims[0]], mean=0, stddev=std, seed=seed))
+    W2 = tf.Variable(tf.random_normal([h_dims[0], h_dims[1]], mean=0, stddev=std, seed=seed))
+    W3 = tf.Variable(tf.random_normal([h_dims[1], train_Y.shape[1]], mean=0, stddev=std, seed=seed))
+    W10 = tf.Variable(tf.zeros([h_dims[0], 1]))
+    W20 = tf.Variable(tf.zeros([h_dims[1], 1]))
+    W30 = tf.Variable(tf.zeros([train_Y.shape[1], 1]))
+
+    train_loss_history = []
+    val_loss_history = []
+    n_incr_error = 0  # nb. of consecutive increase in error
+    best_loss = np.Inf
+    n_batches = train_X.shape[0] / batch_size + (train_X.shape[0] % batch_size != 0)
+
+    h1, h2, y_hat = feedforward(X, W1, W10, W2, W20, W3, W30)
+    cost = calc_loss(Y, y_hat)
+    backprop_op = backprop(X, Y, h1, h2, y_hat, W1, W10, W2, W20, W3, W30, lr)
 
     if shuffle:
         train_X, train_Y = shuffle_data(train_X, train_Y)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        best_it = 0
-        train_loss_history = []
-        val_loss_history = []
-        n_incr_error = 0  # nb. of consecutive increase in error
-        _, _, y_hat = feedforward(X, W1, W10, W2, W20, W3, W30)
-        val_loss = sess.run(calc_loss(Y, y_hat), {X: val_X, Y: val_Y}) # calc init mse
-        best_loss = val_loss
-        best_W = [W1_val, W10_val, W2_val, W20_val, W3_val, W30_val]
-        n_iter = 1
-        n_batches = train_X.shape[0] / batch_size + (train_X.shape[0] % batch_size != 0)
-        while True:
+        import pdb;pdb.set_trace()
+        for n_epoch in range(1, max_epoch + 1):
             n_incr_error += 1
             train_loss = 0.
             val_loss = 0.
             for (batch_X, batch_Y) in next_batch(train_X, train_Y, batch_size):
-                # do gradient descent for one iteration
-                # calc val loss
-                h1, h2, y_hat = feedforward(X, W1, W10, W2, W20, W3, W30)
-                weights_update = backprop(X, Y, h1, h2, y_hat, W1, W10, W2, W20, W3, W30, lr)
-                W_val = sess.run(weights_update, {X: batch_X, Y: batch_Y})
-                _, _, y_hat = feedforward(X, W1, W10, W2, W20, W3, W30)
-                loss = calc_loss(Y, y_hat)
-                train_batch_loss = sess.run(loss, {X: batch_X, Y: batch_Y})
-                val_batch_loss = sess.run(loss, {X: val_X, Y: val_Y})
+                sess.run(backprop_op, {X: batch_X, Y: batch_Y})
+                train_batch_loss = sess.run(cost, {X: batch_X, Y: batch_Y})
+                val_batch_loss = sess.run(cost, {X: val_X, Y: val_Y})
                 train_loss_history.append(train_batch_loss)
                 val_loss_history.append(val_batch_loss)
-                train_loss += train_batch_loss
-                val_loss += val_batch_loss
+                train_loss += train_batch_loss / n_batches
+                val_loss += val_batch_loss / n_batches
 
-            train_loss /= n_batches # average loss
-            val_loss /= n_batches
-
-
-            if val_loss < best_loss:
+            current_loss = val_loss
+            if current_loss - min_delta < best_loss:
                 # update best error (NLL), iteration
-                best_loss = val_loss
-                best_W = W_val
-                best_it = n_iter
+                best_loss = current_loss
+                best_W = sess.run([W1, W10, W2, W20, W3, W30])
+                best_epoch = n_epoch
                 n_incr_error = 0
 
-            if n_iter % print_per_epoch == 0:
-                print 'Iter %s/%s, train loss: %s, val loss: %s' % (n_iter, max_epoch, train_loss, val_loss)
+            if n_epoch % print_per_epoch == 0:
+                print 'Epoch %s/%s, train loss: %s, val loss: %s' % (n_epoch, max_epoch, train_loss, val_loss)
 
             if n_incr_error >= patience:
                 print 'Early stopping occured.'
                 return best_W, best_loss, train_loss_history, val_loss_history
 
-            if n_iter >= max_epoch:
-                print 'Warning: not converged.'
-                return best_W, best_loss, train_loss_history, val_loss_history
-
             if shuffle:
                 train_X, train_Y = shuffle_data(train_X, train_Y)
-            n_iter += 1
+    return best_W, best_loss, train_loss_history, val_loss_history
 
 def predict(X_data, W_val):
     X = tf.placeholder(tf.float32, shape=[None, X_data.shape[1]], name='X')
@@ -245,48 +229,39 @@ def predict(X_data, W_val):
 
     return np.argmax(pred_val, axis=1)
 
-def calc_clf_error(X_data, labels, W, label_lookup):
-    label_lookup_rev = revdict(label_lookup)
+def calc_clf_error(X_data, labels, W):
     pred = predict(X_data, W)
     error_per_class = defaultdict(float)
     count_per_class = defaultdict(float)
+    y = np.argmax(labels, 1)
 
-    for i in range(len(labels)):
-        count_per_class[labels[i]] += 1.
-        if not labels[i] == label_lookup_rev[pred[i]]:
-            error_per_class[labels[i]] += 1.
+    for i in range(len(y)):
+        count_per_class[y[i]] += 1
+        if not y[i] == pred[i]:
+            error_per_class[y[i]] += 1
 
     for each in error_per_class:
         error_per_class[each] /= count_per_class[each]
 
     return dict(error_per_class), np.mean(error_per_class.values())
 
-
-# def plot_weights(W):
-#     n_pixel, k = W.shape
-#     dim = int(np.sqrt(n_pixel - 1))
-#     for i in range(k):
-#         img = W[:-1, i].reshape(dim, dim)
-#         plt.imshow(img)
-#         plt.colorbar()
-#         plt.show()
-
-def plot_loss(train_loss, val_loss, per=5, save_file='loss.png'):
+def plot_loss(train_loss, val_loss, start=50, per=5, save_file='loss.png'):
     assert len(train_loss) == len(val_loss)
-    idx = np.arange(0, len(train_loss), per)
-    plt.plot(idx, train_loss[idx], linestyle='None', alpha=1.0, marker='bs', markersize=6, label='train loss')
-    plt.plot(idx, val_loss[idx], linestyle='None', alpha=1.0, marker='g^', markersize=6, label='val loss')
-    plt.xlabel('# of iter')
+    plt.figure(figsize=(10, 10), facecolor='white')
+    idx = np.arange(start, len(train_loss), per)
+    plt.plot(idx, train_loss[idx], alpha=1.0, label='train loss')
+    plt.plot(idx, val_loss[idx], alpha=1.0, label='val loss')
+    plt.xlabel('# of iteration')
     plt.ylabel('loss')
     legend = plt.legend(loc='upper right', shadow=True)
     plt.savefig(save_file)
-    plt.show()
+    # plt.show()
 
 def train(args):
     X = load_image(args.input) / 255.
     labels = load_label(args.label)
-    Y = one_hot_encode(labels, label_lookup)
-
+    Y = dense_to_one_hot(labels, n_class)
+    assert X.shape[0] == Y.shape[0]
     np.random.seed(0)
     idx = np.arange(X.shape[0])
     val_idx = np.random.choice(idx, args.num_validation, replace=False)
@@ -309,25 +284,20 @@ def train(args):
 
         print 'runtime: %ss' % (timeit.default_timer() - start)
 
-        # if args.plot_weights:
-        #     plot_weights(W)
-        import pdb;pdb.set_trace()
-
         if args.plot_loss:
             plot_loss(train_loss_history, val_loss_history)
+        import pdb;pdb.set_trace()
 
 def test(args):
     X = load_image(args.input) / 255.
-    X = np.append(X, np.ones((X.shape[0], 1)), axis=1).astype('float32')
     labels = load_label(args.label)
+    Y = dense_to_one_hot(labels, n_class)
     W = load_weights(args.load_weights)
+    assert X.shape[0] == Y.shape[0]
 
-    error_per_class, avg_error = calc_clf_error(X, labels, W, label_lookup)
+    error_per_class, avg_error = calc_clf_error(X, Y, W)
     print 'error per class: %s' % error_per_class
     print 'average error: %s' % avg_error
-
-#     # if args.plot_weights:
-#     #     plot_weights(W)
 
 
 
@@ -358,4 +328,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
